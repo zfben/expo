556f99da2f52a96ec55bb58e16d3e79e1ca2b165
evanbacon <bacon@expo.io>
2024-01-16 16:38:07 -0600

    dropping unused

--- apps/rsc-e2e/src/app/index.tsx
+++ apps/rsc-e2e/src/app/index.tsx
@@ -3,8 +3,9 @@ import { Counter } from '../components/Counter';
 const App = ({ name }: { name: string }) => {
   return (
     <div style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}>
-      <title>RSC example</title>
-      <h1>Hello {name}!!</h1>
+      <title>Expo x Metro RSC</title>
+      {/* <h1>Hello {name}!!</h1> */}
+      <h3>Expo x Metro RSC (web) demo.</h3>
       <h3>This is a server component.</h3>
       <Counter />
       <div>{new Date().toISOString()}</div>
