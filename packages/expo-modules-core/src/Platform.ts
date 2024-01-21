46a72f58e668cf1130f29568ffa510a2f6b8e9ee
evanbacon <bacon@expo.io>
2024-01-21 12:54:01 -0600

    add RSC error support

--- packages/expo-modules-core/src/Platform.ts
+++ packages/expo-modules-core/src/Platform.ts
@@ -1,4 +1,4 @@
-import { Platform as ReactNativePlatform, PlatformOSType } from 'react-native';
+import { PlatformOSType } from 'react-native';
 
 import {
   isDOMAvailable,
@@ -11,12 +11,26 @@ export type PlatformSelectOSType = PlatformOSType | 'native' | 'electron' | 'def
 
 export type PlatformSelect = <T>(specifics: { [platform in PlatformSelectOSType]?: T }) => T;
 
+import OS from 'expo-router/os';
+
+function select<T>(specifics: { [platform in PlatformSelectOSType]?: T }): T {
+  if (specifics.hasOwnProperty(OS)) {
+    return specifics[OS]!;
+  } else if (OS !== 'web' && specifics.hasOwnProperty('native')) {
+    return specifics.native!;
+  } else if (specifics.hasOwnProperty('default')) {
+    return specifics.default!;
+  } else {
+    throw new Error(`Platform: '${OS}' is not supported.`);
+  }
+}
+
 const Platform = {
   /**
    * Denotes the currently running platform.
    * Can be one of ios, android, web.
    */
-  OS: ReactNativePlatform.OS,
+  OS: OS,
   /**
    * Returns the value with the matching platform.
    * Object keys can be any of ios, android, native, web, default.
@@ -25,7 +39,7 @@ const Platform = {
    * @android android, native, default
    * @web web, default
    */
-  select: ReactNativePlatform.select as PlatformSelect,
+  select: select as PlatformSelect,
   /**
    * Denotes if the DOM API is available in the current environment.
    * The DOM is not available in native React runtimes and Node.js.
