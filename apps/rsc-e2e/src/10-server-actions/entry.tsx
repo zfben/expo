16ef6ded8b7030af0ff84d9d293be4a442c5697c
evanbacon <bacon@expo.io>
2024-01-20 21:20:10 -0600

    wip - actions

---
+++ apps/rsc-e2e/src/10-server-actions/entry.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from 'react-native';
+
+import { Counter } from './counter';
+import { greet } from './func';
+
+type ServerFunction<T> = T extends (...args: infer A) => infer R
+  ? (...args: A) => Promise<R>
+  : never;
+
+export default function ServerActionTest() {
+  return (
+    <View
+      style={{
+        borderWidth: 3,
+        borderColor: 'green',
+        borderStyle: 'dashed',
+        padding: 8,
+      }}>
+      <Text style={{ fontWeight: 'bold' }}>10) Server Action (Server Component)</Text>
+
+      <Counter greet={greet as unknown as ServerFunction<typeof greet>} />
+      <Text>{new Date().toISOString()}</Text>
+    </View>
+  );
+}
--- apps/rsc-e2e/src/app/index.tsx
+++ apps/rsc-e2e/src/app/index.tsx
@@ -9,6 +9,7 @@ import A06_RSCChildren from '../06-rsc-children/entry';
 import A07_AsyncServerComponent from '../07-async-server-component/entry';
 import A08_NodeBuiltins from '../08-node-builtins/entry';
 import A09_CSSModules from '../09-css-modules/entry';
+import A10_ServerActions from '../10-server-actions/entry';
 
 const Container = OS === 'web' ? (props) => <div {...props} /> : (props) => <View {...props} />;
 
@@ -22,52 +23,36 @@ const App = () => {
 
         <Text>A whole new world.</Text>
       </View>
-      <A02_Suspense />
-      <A03_Promises />
-      <A04_ServerPlatformExtensions />
-      <A05_ClientPlatformExtensions />
-      <A06_RSCChildren />
 
-      <Container>
-        {/* @ts-expect-error */}
-        <A07_AsyncServerComponent />
-      </Container>
-      <A08_NodeBuiltins />
-      <A09_CSSModules />
+      <A10_ServerActions />
     </View>
   );
 };
 
 export default App;
 
-// import { Counter } from '../components/Counter';
-// import { View, Text } from 'react-native-web';
-// import DateStatic from '../components/Date-static';
-// import AsyncBuildData from '../components/AsyncBuildData';
-
-// const App = ({ name }: { name: string }) => {
+// const App = () => {
 //   return (
-//     <View style={{ flex: 1 }}>
-//       <div style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}>
-//         <title>Expo x Metro RSC</title>
-//         {/* <h1>Hello {name}!!</h1> */}
-//         <h3>Expo x Metro RSC (web) demo.</h3>
-//         <h3>This is a server component.</h3>
-//         <DateStatic />
-//         <AsyncBuildData />
-//         <Counter>
-//           <div style={{ border: '3px dodgerblue dashed', margin: '1em', padding: '1em' }}>
-//             <Text>Nested static component child</Text>
-//           </div>
-//         </Counter>
-//         <div style={{ border: '3px purple dashed', margin: '1em', padding: '1em' }}>
-//           <Text>React Native Web client components</Text>
-//         </div>
-//         <div style={{ border: '3px lime dashed', margin: '1em', padding: '1em' }}>
-//           <Text>Secret variable {process.env.EXPO_NOT_PUBLIC_TEST_VALUE}</Text>
-//         </div>
-//         <div>{new Date().toISOString()}</div>
-//       </div>
+//     <View style={{ flex: 1, padding: 16, gap: 8, justifyContent: 'center', alignItems: 'stretch' }}>
+//       <View>
+//         <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+//           Universal React Server Components with Expo Router
+//         </Text>
+
+//         <Text>A whole new world.</Text>
+//       </View>
+//       <A02_Suspense />
+//       <A03_Promises />
+//       <A04_ServerPlatformExtensions />
+//       <A05_ClientPlatformExtensions />
+//       <A06_RSCChildren />
+//       <Container>
+//         {/* @ts-expect-error */}
+//         <A07_AsyncServerComponent />
+//       </Container>
+//       <A08_NodeBuiltins />
+//       <A09_CSSModules />
+//       <A10_ServerActions />
 //     </View>
 //   );
 // };
