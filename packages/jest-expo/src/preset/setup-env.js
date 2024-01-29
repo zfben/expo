74e2417d8cf805c391b6cf112ad9770d9bf22ec0
Cedric van Putten <github@cedric.dev>
2024-01-29 15:11:33 +0100

    docs(jest-expo): add dependency chains for clarity

--- packages/jest-expo/src/preset/setup-env.js
+++ packages/jest-expo/src/preset/setup-env.js
@@ -2,14 +2,22 @@ const path = require('path');
 
 try {
   const expoPath = path.dirname(require.resolve('expo/package.json'));
-  const expoCliPath = path.dirname(require.resolve('@expo/cli/package.json'), { paths: [expoPath] });
+  const expoCliPath = path.dirname(require.resolve('@expo/cli/package.json'), {
+    paths: [expoPath],
+  });
 
-  /** @type {import('@expo/config/paths')} */
+  /**
+   * Dependency chain: expo -> @expo/config
+   * @type {import('@expo/config/paths')}
+   */
   const { getPossibleProjectRoot } = require(
     require.resolve('@expo/config/paths', { paths: [expoPath] })
   );
 
-  /** @type {import('@expo/env')} */
+  /**
+   * Dependency chain: expo -> @expo/cli -> @expo/env
+   * @type {import('@expo/env')}
+   */
   const expoEnv = require(require.resolve('@expo/env', { paths: [expoCliPath] }));
 
   // Auto-load the environment variables from the possible project root
