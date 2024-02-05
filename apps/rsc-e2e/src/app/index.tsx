1869c40fdcecfc7f8445bf63b5ff349d731b2b44
evanbacon <bacon@expo.io>
2024-02-05 12:43:16 -0600

    add ability to prebuild

--- apps/rsc-e2e/src/app/index.tsx
+++ apps/rsc-e2e/src/app/index.tsx
@@ -2,17 +2,17 @@ import React, { Suspense } from 'react';
 import OS from 'expo-router/os';
 // import { Text, View, ScrollView } from 'react-native';
 
-// import A02_Suspense from '../02-suspense/entry';
-// import A03_Promises from '../03-promises/entry';
-// import A04_ServerPlatformExtensions from '../04-server-platform-extensions/entry';
-// import A05_ClientPlatformExtensions from '../05-client-platform-extensions/entry';
-// import A06_RSCChildren from '../06-rsc-children/entry';
-// import A07_AsyncServerComponent from '../07-async-server-component/entry';
-// import A08_NodeBuiltins from '../08-node-builtins/entry';
-// import A09_CSSModules from '../09-css-modules/entry';
-// import A10_ServerActions from '../10-server-actions/entry';
-// import A11_ExpoViewsTest from '../11-expo-views/entry';
-// import A14_RNW from '../14-react-native-web/entry';
+import A02_Suspense from '../02-suspense/entry';
+import A03_Promises from '../03-promises/entry';
+import A04_ServerPlatformExtensions from '../04-server-platform-extensions/entry';
+import A05_ClientPlatformExtensions from '../05-client-platform-extensions/entry';
+import A06_RSCChildren from '../06-rsc-children/entry';
+import A07_AsyncServerComponent from '../07-async-server-component/entry';
+import A08_NodeBuiltins from '../08-node-builtins/entry';
+import A09_CSSModules from '../09-css-modules/entry';
+import A10_ServerActions from '../10-server-actions/entry';
+import A11_ExpoViewsTest from '../11-expo-views/entry';
+import A14_RNW from '../14-react-native-web/entry';
 
 // import { LinearGradient } from 'expo-linear-gradient';
 import { ScrollView, Text, View } from 'react-native';
@@ -38,9 +38,9 @@ const App = () => {
       {/* <Container>
         <A07_AsyncServerComponent />
       </Container> */}
-      {/* <A08_NodeBuiltins /> */}
+      <A08_NodeBuiltins />
       {/* <A09_CSSModules /> */}
-      {/* <A10_ServerActions /> */}
+      <A10_ServerActions />
       {/* <A11_ExpoViewsTest /> */}
       {/* <A14_RNW /> */}
     </ScrollView>
