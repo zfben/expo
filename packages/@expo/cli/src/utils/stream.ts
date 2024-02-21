c92167b2ec82bc410f4f3edc33d37186467a9406
evanbacon <bacon@expo.io>
2024-02-21 14:36:38 -0600

    Update stream.ts

--- packages/@expo/cli/src/utils/stream.ts
+++ packages/@expo/cli/src/utils/stream.ts
@@ -1,44 +1,45 @@
-function decodeStream(textDecoder = new TextDecoder()) {
-  return new TransformStream<Uint8Array, string>({
-    transform(chunk, controller) {
-      return controller.enqueue(textDecoder.decode(chunk, { stream: true }));
-    },
-    flush(controller) {
-      return controller.enqueue(textDecoder.decode());
-    },
-  });
+// function decodeStream(textDecoder = new TextDecoder()) {
+//   return new TransformStream<Uint8Array, string>({
+//     transform(chunk, controller) {
+//       return controller.enqueue(textDecoder.decode(chunk, { stream: true }));
+//     },
+//     flush(controller) {
+//       return controller.enqueue(textDecoder.decode());
+//     },
+//   });
+// }
+
+export async function streamToStringAsync(stream: ReadableStream): Promise<string> {
+  const decoder = new TextDecoder();
+  const reader = stream.getReader();
+  const outs: string[] = [];
+  let result: ReadableStreamReadResult<unknown>;
+  do {
+    result = await reader.read();
+    if (result.value) {
+      if (!(result.value instanceof Uint8Array)) {
+        throw new Error('Unexepected buffer type');
+      }
+      outs.push(decoder.decode(result.value, { stream: true }));
+    }
+  } while (!result.done);
+  outs.push(decoder.decode());
+  return outs.join('');
 }
 
-// const streamToString = async (stream: ReadableStream): Promise<string> => {
-//     const decoder = new TextDecoder();
-//     const reader = stream.getReader();
-//     const outs: string[] = [];
-//     let result: ReadableStreamReadResult<unknown>;
-//     do {
-//       result = await reader.read();
-//       if (result.value) {
-//         if (!(result.value instanceof Uint8Array)) {
-//           throw new Error('Unexepected buffer type');
-//         }
-//         outs.push(decoder.decode(result.value, { stream: true }));
-//       }
-//     } while (!result.done);
-//     outs.push(decoder.decode());
-//     return outs.join('');
-//   };
-export async function streamToStringAsync(stream: ReadableStream<Uint8Array>) {
-  let output = '';
+// export async function streamToStringAsync(stream: ReadableStream<Uint8Array>) {
+//   let output = '';
 
-  await stream
-    // Decode the streamed chunks to turn them into strings.
-    .pipeThrough(decodeStream())
-    .pipeTo(
-      new WritableStream<string>({
-        write(chunk) {
-          output += chunk;
-        },
-      })
-    );
+//   await stream
+//     // Decode the streamed chunks to turn them into strings.
+//     .pipeThrough(decodeStream())
+//     .pipeTo(
+//       new WritableStream<string>({
+//         write(chunk) {
+//           output += chunk;
+//         },
+//       })
+//     );
 
-  return output;
-}
+//   return output;
+// }
