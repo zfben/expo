65c8f40ca4f5f64be1d88cbb5956561c02f67dc0
evanbacon <bacon@expo.io>
2024-02-28 11:02:37 -0600

    add qualified root

--- packages/expo-router/build/qualified-entry.js
+++ packages/expo-router/build/qualified-entry.js
@@ -10,83 +10,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 // This has to be the string "expo-router/_ctx" as we resolve the exact string to
 // a different file in a custom resolver for bundle splitting in Node.js.
-const getDevServer_1 = __importDefault(require("@expo/metro-runtime/build/getDevServer"));
+// import getDevServer from '@expo/metro-runtime/build/getDevServer';
+// import { ctx } from 'expo-router/_ctx';
 const react_1 = __importDefault(require("react"));
 const react_native_safe_area_context_1 = require("react-native-safe-area-context");
 const exports_1 = require("./exports");
-const head_1 = require("./head");
 // MUST be the one from metro-runtime as it contains the URL query parameters for the bundle to configure Metro.
 const client_1 = require("./rsc/router/client");
 const Try_1 = require("./views/Try");
-// import { Router } from './rsc/router/client';
-// import ContextNavigator from './rsc-navigator';
-// import { ExpoRoot } from './ExpoRoot';
-const introUrl = (0, getDevServer_1.default)().fullBundleUrl;
-// TODO: This is buggy and doesn't work well, maybe inject the query params in babel.
-const searchParams = introUrl ? new URL(introUrl).searchParams.toString() : '';
-// console.log('searchParams', searchParams);
+const react_native_1 = require("react-native");
 // Must be exported or Fast Refresh won't update the context
 function App() {
-    // console.log('ctx', ctx.keys());
-    // {/* <ExpoRoot context={ctx} /> */}
-    // return (
-    //   <View style={{ flex: 1 }}>
-    //     <Text>Hey</Text>
-    //   </View>
-    // );
-    // console.log('Mount')
-    // return (
-    //   <Text>HeyHeyHeyHeyHeyHey</Text>
-    // )
-    return (<react_1.default.Suspense fallback={null}>
-      <head_1.Head.Provider>
-        <react_native_safe_area_context_1.SafeAreaProvider>
-          <Try_1.Try catch={exports_1.ErrorBoundary}>
-            <client_1.Router />
-          </Try_1.Try>
-        </react_native_safe_area_context_1.SafeAreaProvider>
-      </head_1.Head.Provider>
+    return (<react_1.default.Suspense fallback={<react_native_1.Text style={{
+                marginHorizontal: 8,
+                marginTop: 56,
+                padding: 12,
+                fontSize: 16,
+                borderColor: 'blue',
+                borderWidth: 2,
+            }}>
+          [Root Suspense Boundary]
+        </react_native_1.Text>}>
+      <react_native_safe_area_context_1.SafeAreaProvider>
+        <Try_1.Try catch={exports_1.ErrorBoundary}>
+          <client_1.Router />
+        </Try_1.Try>
+      </react_native_safe_area_context_1.SafeAreaProvider>
     </react_1.default.Suspense>);
-    // return (
-    //   <React.Suspense fallback={null}>
-    //     <Head.Provider>
-    //       <SafeAreaProvider>
-    //         <Try catch={ErrorBoundary}>
-    //           <Root initialInput="layout" initialSearchParamsString={searchParams}>
-    //             <Slot id={'layout/page'} />
-    //           </Root>
-    //         </Try>
-    //       </SafeAreaProvider>
-    //     </Head.Provider>
-    //   </React.Suspense>
-    // );
-    // return (
-    //   <React.Suspense fallback={null}>
-    //     <Head.Provider>
-    //       <SafeAreaProvider>
-    //         <ContextNavigator>
-    //           <Try catch={ErrorBoundary}>
-    //             <Router />
-    //             {/* <Root initialSearchParamsString={searchParams}> */}
-    //             {/* <Slot id={input} /> */}
-    //             {/* </Root> */}
-    //           </Try>
-    //         </ContextNavigator>
-    //       </SafeAreaProvider>
-    //     </Head.Provider>
-    //   </React.Suspense>
-    // );
-    // return (
-    //   <React.Suspense fallback={null}>
-    //     <Head.Provider>
-    //       <Try catch={ErrorBoundary}>
-    //         <Router>
-    //           <ExpoRoot context={ctx} />
-    //         </Router>
-    //       </Try>
-    //     </Head.Provider>
-    //   </React.Suspense>
-    // );
 }
 exports.App = App;
 //# sourceMappingURL=qualified-entry.js.map
--- packages/expo-router/src/qualified-entry.tsx
+++ packages/expo-router/src/qualified-entry.tsx
@@ -5,96 +5,41 @@
 
 // This has to be the string "expo-router/_ctx" as we resolve the exact string to
 // a different file in a custom resolver for bundle splitting in Node.js.
-import getDevServer from '@expo/metro-runtime/build/getDevServer';
-import { ctx } from 'expo-router/_ctx';
+// import getDevServer from '@expo/metro-runtime/build/getDevServer';
+// import { ctx } from 'expo-router/_ctx';
 import React from 'react';
 
-// import { ExpoRoot } from './ExpoRoot';
-import { Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { ErrorBoundary } from './exports';
-import { Head } from './head';
-import { Slot, Root } from './rsc/client';
 
 // MUST be the one from metro-runtime as it contains the URL query parameters for the bundle to configure Metro.
 import { Router } from './rsc/router/client';
 import { Try } from './views/Try';
-// import { Router } from './rsc/router/client';
-// import ContextNavigator from './rsc-navigator';
-// import { ExpoRoot } from './ExpoRoot';
+import { Text } from 'react-native';
 
-const introUrl = getDevServer().fullBundleUrl;
-// TODO: This is buggy and doesn't work well, maybe inject the query params in babel.
-const searchParams = introUrl ? new URL(introUrl).searchParams.toString() : '';
-
-// console.log('searchParams', searchParams);
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  // console.log('ctx', ctx.keys());
-  // {/* <ExpoRoot context={ctx} /> */}
-
-  // return (
-  //   <View style={{ flex: 1 }}>
-  //     <Text>Hey</Text>
-  //   </View>
-  // );
-  // console.log('Mount')
-  // return (
-  //   <Text>HeyHeyHeyHeyHeyHey</Text>
-  // )
-
   return (
-    <React.Suspense fallback={null}>
-      <Head.Provider>
-        <SafeAreaProvider>
-          <Try catch={ErrorBoundary}>
-            <Router />
-          </Try>
-        </SafeAreaProvider>
-      </Head.Provider>
+    <React.Suspense
+      fallback={
+        <Text
+          style={{
+            marginHorizontal: 8,
+            marginTop: 56,
+            padding: 12,
+            fontSize: 16,
+            borderColor: 'blue',
+            borderWidth: 2,
+          }}>
+          [Root Suspense Boundary]
+        </Text>
+      }>
+      <SafeAreaProvider>
+        <Try catch={ErrorBoundary}>
+          <Router />
+        </Try>
+      </SafeAreaProvider>
     </React.Suspense>
   );
-
-  // return (
-  //   <React.Suspense fallback={null}>
-  //     <Head.Provider>
-  //       <SafeAreaProvider>
-  //         <Try catch={ErrorBoundary}>
-  //           <Root initialInput="layout" initialSearchParamsString={searchParams}>
-  //             <Slot id={'layout/page'} />
-  //           </Root>
-  //         </Try>
-  //       </SafeAreaProvider>
-  //     </Head.Provider>
-  //   </React.Suspense>
-  // );
-
-  // return (
-  //   <React.Suspense fallback={null}>
-  //     <Head.Provider>
-  //       <SafeAreaProvider>
-  //         <ContextNavigator>
-  //           <Try catch={ErrorBoundary}>
-  //             <Router />
-  //             {/* <Root initialSearchParamsString={searchParams}> */}
-  //             {/* <Slot id={input} /> */}
-  //             {/* </Root> */}
-  //           </Try>
-  //         </ContextNavigator>
-  //       </SafeAreaProvider>
-  //     </Head.Provider>
-  //   </React.Suspense>
-  // );
-  // return (
-  //   <React.Suspense fallback={null}>
-  //     <Head.Provider>
-  //       <Try catch={ErrorBoundary}>
-  //         <Router>
-  //           <ExpoRoot context={ctx} />
-  //         </Router>
-  //       </Try>
-  //     </Head.Provider>
-  //   </React.Suspense>
-  // );
 }
